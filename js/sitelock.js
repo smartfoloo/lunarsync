f6c715401f87bf65e7b57b19590faaea362ed042
platformerdotio <smartfoloo@gmail.com>
2024-02-16 23:06:34 -0600

    update

--- js/sitelock.js
+++ js/sitelock.js
@@ -1,28 +1,8 @@
-var allowedDomains = [
-  "https://stain.espacoextra.pt",
-  "https://zoology.isyour.guru",
-  "https://cloud.espacoextra.pt",
-  "https://bumblebee.kooh.cl",
-  "https://ecology.miboletonic.com",
-  "https://skate.tomahawkchurch.org",
-  "https://geography.towerchile.cl",
-  "https://stain.espacoextra.pt",
-  "https://cable.sanluix.org",
-  "https://light.prt-argentina.org.ar",
-  "https://bread.hancockcountyfoodpantry.com",
-  "https://state.towerchile.cl",
-  "https://snack.prt-argentina.org.ar",
-  "https://terrain.fenesisu.moe",
-  "https://physics.asoch.cl",
-  "https://helloguyspleasestopstealingthis.youramys.com"
-];
+var allowedDomain = window.location.hostname;
 
 var referringUrl = document.referrer;
+var referringHostname = new URL(referringUrl).hostname;
 
-var isAllowedDomain = allowedDomains.some(function (domain) {
-  return referringUrl.indexOf(domain) !== -1;
-});
-
-if (!isAllowedDomain) {
+if (referringHostname !== allowedDomain) {
   window.location.href = "https://helloskids.pages.dev";
 }
