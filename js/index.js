be57224273788d840246d4ac99cddc8305cb24db
platformerdotio <smartfoloo@gmail.com>
2024-02-23 00:08:51 -0600

    ok

--- js/index.js
+++ js/index.js
@@ -45,19 +45,27 @@ function removeLazyLoading() {
 }
 
 if (localStorage.getItem("aboutblankEnabled") === "true") {
-  const popup = open("about:blank", "_blank")
-  const document = popup.document
-  const body = document.body
-  const bodystyle = body.style
-  const iframe = document.createElement('iframe')
-  const iframestyle = iframe.style
-  iframe.src = location.href
-  iframestyle.top = iframestyle.bottom = iframestyle.left = iframestyle.right = 0
-  iframestyle.border = iframestyle.outline = 'none'
-  iframestyle.width = iframestyle.height = '100%'
-  bodystyle.margin = bodystyle.padding = '0'
-  document.body.appendChild(iframe)
-  location.replace('https://classroom.google.com')
+  let iFramed
+  try {
+    iFramed = window !== top
+  } catch (e) {
+    iFramed = true
+  }
+  if (!iFramed) {
+    const popup = open("about:blank", "_blank")
+    const document = popup.document
+    const body = document.body
+    const bodystyle = body.style
+    const iframe = document.createElement('iframe')
+    const iframestyle = iframe.style
+    iframe.src = location.href
+    iframestyle.top = iframestyle.bottom = iframestyle.left = iframestyle.right = 0
+    iframestyle.border = iframestyle.outline = 'none'
+    iframestyle.width = iframestyle.height = '100%'
+    bodystyle.margin = bodystyle.padding = '0'
+    document.body.appendChild(iframe)
+    location.replace('https://classroom.google.com/');
+  }
 }
 
 function startTrackingTime() {
