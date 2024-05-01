50795152102dd625b316498b388899f988e562fe
platformerdotio <smartfoloo@gmail.com>
2024-04-30 23:59:14 -0500

    space wars battleground

---
+++ public/assets/fonts/space-wars-battleground/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "GoGoMan-Space Wars Battleground-0.1";
+const contentToCache = [
+    "Build/SpaceWars_Yandex.loader.js",
+    "Build/SpaceWars_Yandex.framework.js.br",
+    "Build/SpaceWars_Yandex.data.br",
+    "Build/SpaceWars_Yandex.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
---
+++ public/assets/fonts/space-wars-battleground/sdk.js
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>NoSuchKey</Code><Message>The specified key does not exist.</Message><Resource>/289661/4y9k5yzre8v4owflwxfg4fwu6id5wtw6/sdk.js</Resource><RequestId>060c926000f356b9</RequestId></Error>
